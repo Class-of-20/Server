0463be28cca218421fea1f0fbc32c794fc169b45
yejin-e <yejin01e@gmail.com>
2023-11-28 03:05:25 +0900

    [Chore] 로컬 포트 번호 변경

--- app.js
+++ app.js
@@ -25,6 +25,6 @@ app.use('/login', require('./Router/loginRouter'));
 
 app.use('/post', require('./Router/postRouter'));
 
-app.listen(8080, () => {
-    console.log("Server is running on port 8080.");
+app.listen(80, () => {
+    console.log("Server is running on port 80.");
 });
