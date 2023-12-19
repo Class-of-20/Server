040de3253545d3c3da5fd35e98f50f2e9c0662b7
wive10 <20203197@office.deu.ac.kr>
2023-12-19 15:57:51 +0900

    push 오류 수정

--- app.js
+++ app.js
@@ -7,38 +7,45 @@ require("dotenv").config();
 
 const app = express();
 const port = process.env.PORT;
-const server = http.createServer(app);
 
 //미틀웨어 및 라우터 설정
 app.use(express.json());
-app.use(session({
-    key: 'sessionIdx',
-    secret: 'secret',
-    resave: false,
-    saveUninitialized: false,
-    cookie: {
-        maxAge: 24000 * 60 * 60     // 쿠키 유효기간 24시간
-    }
-}));
 app.use('/user', require('./Router/userRouter'));
 app.use('/post', require('./Router/postRouter'));
 
-//소켓 설정
-const io = new Server(server, {
-    cors: {
-      origin: "http://cloud.swdev.kr:4007",
-      methods: ["GET", "POST"]
-    }
-  }
-);
-require("./Service/io")(io);
-app.get("/room/:idx", (req, res) => {
-    console.log("/room/:idx 연결")
-    const idx = req.params.idx;
-    res.send(`Room ${idx}에 대한 정보를 보여주는 페이지`);
-  }
-);
-
-app.listen(port, () => {
-    console.log("Server is running on port", port);
+/*==================================*/ 
+const server = app.listen(port, () => {
+  console.log("Server is running on port",port);
 });
+/*=======================================*/
+//서버에  socket io 추가
+const io = require('socket.io')(server)
+const connectedUser = new Set();
+
+//io.on(이벤트 명, 콜백함수)=> 이벤트 수신
+io.on('connection', (socket)=>{
+  //채팅방 연결 시, 
+  console.log("connected Successfully", socket.id);
+  connectedUser.add(socket.id);
+  io.emit('connected-user',connectedUser.size);
+  //채팅방 연결 해제 시,
+  socket.on('disconnect', ()=>{
+      console.log("disconnected Successfully", socket.id);
+      connectedUser.delete(socket.id);
+      io.emit('connected-user',connectedUser.size);
+  });
+  //메세지 보낼 시,
+  socket.on('message', (data)=>{
+      console.log(data);   
+      //해당 소켓을 제외한 나머지 소켓에세 데이터를 전송
+      socket.emit('message-recive', data);
+  })
+  // "joinChatRoom" 이벤트가 발생하면 콜백 함수 실행
+  socket.on("joinChatRoom", async () => {
+      // 현재 소켓을 "chatRoom UUID"에 조인
+      console.log("joinchatroom");
+      socket.join("chatRoom UUID");
+      // "chatRoom UUID"에 있는 모든 소켓에게 "hello" 이벤트 emit
+      io.to("chatRoom UUID").emit("hello");
+  });
+})
