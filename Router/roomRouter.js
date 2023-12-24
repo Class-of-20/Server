f3019e59f1d4f13608d2c21379b4142aa5811ebc
wive10 <20203197@office.deu.ac.kr>
2023-12-25 04:21:52 +0900

    Update roomRouter.js
    라우터 readroom의 라우터 수정 및 joinRoom의 라우터 /join 추가

--- Router/roomRouter.js
+++ Router/roomRouter.js
@@ -3,13 +3,10 @@ const router = express.Router();
 
 const userController = require('../Service/roomService');
 
-// '/room' is the default endpoint
-router.get('/', (req, res, next) => {
-    console.log("default endpoint");
-});
-
 // 채팅방 정렬
-router.get('/:user_idx',userController.readRoom);
+router.get('/',userController.readRoom);
 
+//채팅방 참여
+router.get('/join', userController.joinRoom);
 
 module.exports = router;
