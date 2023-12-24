939165e02a319c6a994a8247a5ac3a8f6bed2ab7
subinth <a6929ab@naver.com>
2023-12-25 01:13:32 +0900

    [Feat] createRoom
    채팅방 생성

--- Router/roomRouter.js
+++ Router/roomRouter.js
@@ -8,6 +8,9 @@ router.get('/', (req, res, next) => {
     console.log("default endpoint");
 });
 
+// 채팅방 생성
+router.post('/create', userController.createRoom);
+
 // 채팅방 목록
 router.get('/:user_idx',userController.readRoom);
 
